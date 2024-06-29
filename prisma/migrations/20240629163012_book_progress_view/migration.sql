e82a41bf1f874f8526e8a5062f2c919851702b51
Xetera <contact@xetera.dev>
2024-06-29 19:30:46 +0300

    fix: add view migration

---
+++ prisma/migrations/20240629163012_book_progress_view/migration.sql
@@ -0,0 +1,10 @@
+-- This is an empty migration.
+
+CREATE VIEW book_progress_view AS
+SELECT bp.book_id, bp.progress, bp.device, bp.sync_date
+FROM (
+  SELECT *,
+        ROW_NUMBER() OVER (PARTITION BY book_id ORDER BY sync_date DESC, progress DESC) AS rn
+  FROM book_progress
+) bp
+WHERE bp.rn = 1;
